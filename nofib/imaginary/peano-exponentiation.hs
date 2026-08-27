83adb6dff3cd0544149c4338c2ac46dcc844ad06
Louis F. Ashfield <louis.ashfield@pm.me>
2026-08-27 23:23:28 +0100

    Port the Peano exponentiation and Takeuchi benchmarks

---
+++ nofib/imaginary/peano-exponentiation.hs
@@ -0,0 +1,39 @@
+-- Low sharing, no parallelisme; mirrors `peano-exponentiation.rete`.
+
+-- \$ fourmolu --mode inplace nofib/imaginary/peano-exponentiation.hs
+
+import Data.Word (Word64)
+import System.Environment (getArgs)
+
+data Nat = Z | S Nat
+
+defaultInput :: Word64
+defaultInput = 8
+
+main :: IO ()
+main = do
+    args <- getArgs
+    let n = case args of
+            [] -> defaultInput
+            (input : _) -> read input
+    print (int (pow (nat 3) (nat n)))
+
+nat :: Word64 -> Nat
+nat n =
+    if n < 1 then Z else S (nat (n - 1))
+
+int :: Nat -> Word64
+int x =
+    case x of Z -> 0; S x -> 1 + int x
+
+add :: Nat -> Nat -> Nat
+add x y =
+    case x of Z -> y; S x -> S (add x y)
+
+mul :: Nat -> Nat -> Nat
+mul x y =
+    case y of Z -> Z; S y -> add (mul x y) x
+
+pow :: Nat -> Nat -> Nat
+pow x y =
+    case y of Z -> S Z; S y -> mul x (pow x y)
---
+++ nofib/imaginary/takeuchi.hs
@@ -0,0 +1,29 @@
+-- No sharing, maximal parallelisme; mirrors `takeuchi.rete`.
+
+-- \$ fourmolu --mode inplace nofib/imaginary/takeuchi.hs
+
+import Data.Int (Int64)
+import System.Environment (getArgs)
+
+defaultX :: Int64
+defaultX = 31
+
+defaultY :: Int64
+defaultY = 16
+
+defaultZ :: Int64
+defaultZ = 8
+
+main :: IO ()
+main = do
+    args <- getArgs
+    let (x, y, z) = case args of
+            (a : b : c : _) -> (read a, read b, read c)
+            _ -> (defaultX, defaultY, defaultZ)
+    print (tak x y z)
+
+tak :: Int64 -> Int64 -> Int64 -> Int64
+tak x y z =
+    if not (y < x)
+        then z
+        else tak (tak (x - 1) y z) (tak (y - 1) z x) (tak (z - 1) x y)
